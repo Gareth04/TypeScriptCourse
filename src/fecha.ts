c9568aa28e6705047fc3babbcffdcab89923ddef
Bryan Hidalgo <stevenhidalgo_10@hotmail.com>
2024-12-10 00:16:34 -0500

    Ajuste envio de personas

--- src/fecha.ts
+++ src/fecha.ts
@@ -6,3 +6,17 @@ let numero: number = 12;
 let boolean: boolean = true;
 let cualquierCosa: any = "Cualquier cosa";
 console.log(fecha.getDate() + "/" +(fecha.getMonth()+1) +"/" + fecha.getFullYear());
+
+
+//Arrays
+
+let persona: string[] = ["bryan","nestor","christopher","kone"]
+
+//Que tipo de dato tiene un elemento HTML, es de tipo HTMLELEMENT
+let div_personas:HTMLElement | null = document.querySelector("#personas");
+
+div_personas.innerHTML = "<ul>";
+persona.map((persona)=>{
+    div_personas.innerHTML += `<li> ${persona} </li>`; //Igualar  el tipo de dato de persona de string al valor que va dentro de un innerHTML
+})
+div_personas.innerHTML += "<ul>";
